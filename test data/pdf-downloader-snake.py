8f7bce3d379f86e0c4ed32d63b6d5a094fd2ed69
Yorl <94957768+DataFromBelow@users.noreply.github.com>
2026-02-25 14:35:23 +0100

    Update pdf-downloader-snake.py

--- test data/pdf-downloader-snake.py
+++ test data/pdf-downloader-snake.py
@@ -5,10 +5,13 @@ Created on Monday Feb 23 2026
 """
 
 
+import os
+import os.path
 import wget
 import pandas as pd
-import numpy
+import numpy as np
 import requests
+import glob
 
 
 
@@ -16,14 +19,60 @@ dwn_url = ""
 
 
 # This is the input file path for the excel file containing the url's 
-input_path = "\Data\test-data.xlsx"
-
-# This is the output file path 
-output_path = ""
+input_path = "C:/Users/SPAC-49/Documents/Specialisterne/Softwareudvikling/Uge 4/test data/test-data.xlsx"
 
 ID = "BRnum"
 
-df = pd.read_excel(input_path, sheet_name=0, index_col=ID)
+try:
+    df = pd.read_excel(input_path, sheet_name=0)
+    #print(df)
+    print(df["Pdf_URL"][2])
+except:
+    print("GODDAMNIT")
+finally:
+    print("test ended")
+
+
+
+
+
+# This is the output file path 
+output_path = "output/"
+
+for j in range(len(df)):
+    
+    try:
+        file_name = df["BRnum"][j]
+        url_link = df["Pdf_URL"][j]
+        #print(file_name + ".pdf")
+    except:
+        print("lol")
+    finally:
+        pass
+
+    if type(url_link) is float:
+        print("Nan")
+        pass
+    elif type(url_link) is str:
+        print(url_link)
+        try:
+            response = requests.get(url_link)
+            if response.status_code == 200:
+                #os.path and os goes here
+                # this is where you'd take data and store as a pdf in output path 
+                print("200")
+            else:
+                print("404")
+            pass
+        except:
+            pass
+    else:
+        print("fail")
+    
+
+
+
+
 
 # function for filtering rows with no url's
 # function goes here
@@ -33,11 +82,4 @@ df = pd.read_excel(input_path, sheet_name=0, index_col=ID)
 # not sure how to check that
 duplicate_check = ""
 
-#try:
-#
-#except :
 
-with open(input_path, 'r') as dummy_data:
-    
-
-    pass
