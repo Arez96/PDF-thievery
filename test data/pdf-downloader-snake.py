b8152d5508669c61c6f821e8682f33d94bc2cb30
Yorl <94957768+DataFromBelow@users.noreply.github.com>
2026-02-27 14:40:08 +0100

    Update pdf-downloader-snake.py

--- test data/pdf-downloader-snake.py
+++ test data/pdf-downloader-snake.py
@@ -6,15 +6,12 @@ Created on Monday Feb 23 2026
 
 import os
 import os.path
-import wget
+#import wget
 import pandas as pd
-import numpy as np
+#import numpy as np
 import requests
-import glob
+#import glob
 
-
-
-dwn_url = ""
 
 
 # This is the input file path for the excel file containing the url's 
@@ -23,9 +20,9 @@ input_path = "C:/Users/SPAC-49/Documents/Specialisterne/Softwareudvikling/Uge 4/
 # This is the output file path 
 output_path = "C:/Users/SPAC-49/Documents/Specialisterne/Softwareudvikling/Uge 4/test data/output"
 
-ID = "BRnum"
+# ID = "BRnum"
 
-try:
+try: # Checks if the list has even loaded
     df = pd.read_excel(input_path, sheet_name=0)
     #print(df)
     #print(df["Pdf_URL"][2])
@@ -36,41 +33,24 @@ finally:
 
 
 
-
-
 for j in range(len(df)):
-    
-    try:
-        file_name = df["BRnum"][j]
-        url_link = df["Pdf_URL"][j]
-        #print(file_name + ".pdf")
-    except:
-        print("critical failure")
-    finally:
+    file_name = df["BRnum"][j]
+    url_link = df["Pdf_URL"][j]
+    if type(url_link) is float: # Solves NaN as a problem
+        print("NaN")
         pass
-    
-    if type(url_link) is float:
-        print("Nan")
-        pass
-    elif type(url_link) is str and os.path.exists(f"{output_path}/{file_name}.pdf") == False:
-        #print(url_link)
-        try: #I know what i need to do. Use os to scan if the file is there already
-            #if :
-            #else:
+    elif type(url_link) is str and os.path.exists(f"{output_path}/{file_name}.pdf") == False: # checks for url_link and the file not existing, does not overwrite files
+        #print(url_link) # error checking
+        try: 
             response = requests.get(url_link, allow_redirects=True)
             mime_type = str(response.headers.get("Content-Type"))
-            if response.status_code == 200 and mime_type == "application/pdf":
-                #os.path and os goes here
-                # this is where you'd take data and store as a pdf in output path
-                #print("200")
+            if response.status_code == 200 and mime_type == "application/pdf": # checks for response code 200 and mime type application/pdf 
                 print(f"200 : {file_name} / ({mime_type}): Success")
-                #grab the contents
                 save_file = str(file_name + ".pdf")
                 save_dest = os.path.join(output_path, save_file)
-                #save response.content
                 try:
-                    with open(save_dest, "wb") as file: 
-                        file.write(response.content)
+                    with open(save_dest, "wb") as file: #grab the contents
+                        file.write(response.content) #save response.content
                 except:
                     print(f"Unknown Failure: {save_file}")
             elif response.status_code == 404:
@@ -88,17 +68,23 @@ for j in range(len(df)):
         # print("fail")
     
 
+#testing area
+
+#    try:
+#        file_name = df["BRnum"][j]
+#        url_link = df["Pdf_URL"][j]
+#        #print(file_name + ".pdf")
+#    except:
+#        print("critical failure")
+#    finally:
+#        pass
+
 # function for filtering rows with no url's
 # function goes here
 
 # we gotta check if it has already been downloaded
 # duplicate check
 # not sure how to check that
-duplicate_check = ""
-
-
-
-
-
+#duplicate_check = ""
 
 
